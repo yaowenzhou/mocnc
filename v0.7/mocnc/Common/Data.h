7f8b4585cfe6b1524061cf226821f065e12445e4
yaowenzhou <yaowenzhou@foxmail.com>
2021-04-02 15:36:53 +0800

    将服务端升级为select模型

---
+++ v0.7/mocnc/Common/Data.h
@@ -0,0 +1,46 @@
+#pragma once
+
+enum CMD {
+  CMD_LOGIN,
+  CMD_LOGIN_RESULT,
+  CMD_LOGOUT,
+  CMD_LOGOUT_RESULT,
+  CMD_ERROR
+};
+
+struct DataHeader {
+  int maxLength;
+  int dataLength;
+  int cmd;
+};
+struct Login : public DataHeader {
+  Login() {
+    dataLength = sizeof(Login);
+    cmd = CMD_LOGIN;
+  }
+  char userName[32];
+  char password[32];
+};
+struct LoginResult : public DataHeader {
+  LoginResult() {
+    dataLength = sizeof(LoginResult);
+    cmd = CMD_LOGIN_RESULT;
+    result = 0;
+  }
+  int result;
+};
+struct Logout : public DataHeader {
+  Logout() {
+    dataLength = sizeof(Logout);
+    cmd = CMD_LOGOUT;
+  }
+  char userName[32];
+};
+struct LogoutResult : public DataHeader {
+  LogoutResult() {
+    dataLength = sizeof(LogoutResult);
+    cmd = CMD_LOGOUT_RESULT;
+    result = 0;
+  }
+  int result;
+};
---
+++ v0.7/readme.md
@@ -0,0 +1,10 @@
+<!--
+ * @Author       : yaowenzhou
+ * @Date         : 2021-03-31 16:05:50
+ * @LastEditors  : yaowenzhou
+ * @LastEditTime : 2021-04-01 16:06:09
+ * @version      : 
+ * @Description  : 
+-->
+C++ 百万并发通信 V0.7
+服务端升级为select模型处理多客户端。
