cfb0630e485b95714246574f67ed2e5e58fcb5de
yaowenzhou <yaowenzhou@foxmail.com>
2021-04-18 17:21:12 +0800

    Adapt to Linux platform.

--- v1.0/Client/main.cpp
+++ v1.0/Client/main.cpp
@@ -13,9 +13,9 @@
 #include <WinSock2.h>
 #include <Windows.h>
 #else
-#include <unistd.h>
 #include <arpa/inet.h>
-#include <string.h>
+#include <unistd.h>
+#include <cstring>
 #define SOCKET int
 #define INVALID_SOCKET (SOCKET)(~0)
 #define SOCKET_ERROR (-1)
@@ -26,82 +26,67 @@
 #include <thread>
 
 #include "../Common/Data.h"
+#include "../Common/cross_platform_socket.h"
+#include "../Common/log.h"
 
-#define SERVER_IP "192.168.1.2"
+#define SERVER_IP "127.0.0.1"
 
 // 加入WindowsSocket的静态库，此处采取第二种方法，为工程添加附加依赖项
 // #pragma comment(lib, "ws2_32.lib")
 
 // 通信socket的读取函数，专门用来读取从服务端发回来的消息
-int doRead(SOCKET _cSock)
-{
+int doRead(SOCKET _cSock) {
   char recvBuf[1024] = {};
   // 5. 接收服务器数据，如果接收到的数据长度为0，则退出函数
-  if (recv(_cSock, recvBuf, sizeof(DataHeader), 0) <= 0)
-  {
-#ifdef WIN32
-    printf("ERROR|recv error: %u.\n", GetLastError());
-#else
-    printf("ERROR|recv error: %d.\n", errno);
-#endif
+  if (recv(_cSock, recvBuf, sizeof(DataHeader), 0) <= 0) {
+    logError(__FILE__, __LINE__, "recv");
     return -1;
   }
-  DataHeader *header = (DataHeader *)recvBuf;
-  switch (header->cmd)
-  {
-  case CMD::CMD_LOGIN_RESULT:
-  {
-    // 由于Login中包含了DataHeader，第二次接收时，需要进行内存偏移
-    recv(_cSock, recvBuf + sizeof(DataHeader),
-         header->dataLength - sizeof(DataHeader), 0);
-    LoginResult *result = (LoginResult *)recvBuf;
-    printf(
-        "INFO|Receive login result from server, data length: %d, result "
-        "code: %d.\n",
-        result->dataLength, result->result);
-  }
-  break;
-  case CMD::CMD_LOGOUT_RESULT:
-  {
-    recv(_cSock, recvBuf + sizeof(DataHeader),
-         header->dataLength - sizeof(DataHeader), 0);
-    LogoutResult *result = (LogoutResult *)recvBuf;
-    printf(
-        "INFO|Receive logout result from server, data length: %d, result "
-        "code: "
-        "%d\n",
-        result->dataLength, result->result);
-  }
-  break;
-  case CMD::CMD_NEW_USER_JOIN:
-  {
-    recv(_cSock, recvBuf + sizeof(DataHeader),
-         header->dataLength - sizeof(DataHeader), 0);
-    NewUserJoin *newJoin = (NewUserJoin *)recvBuf;
-    printf(
-        "INFO|Receive new user join from server, data length: %d, new user: "
-        "%d.\n",
-        newJoin->dataLength, newJoin->sock);
-  }
-  break;
-  default:
-  {
-    DataHeader header = {0, CMD_ERROR};
-    printf("INFO|Receive unknow command.\n");
-  }
-  break;
+  DataHeader* header = (DataHeader*)recvBuf;
+  switch (header->cmd) {
+    case CMD::CMD_LOGIN_RESULT: {
+      // 由于Login中包含了DataHeader，第二次接收时，需要进行内存偏移
+      recv(_cSock, recvBuf + sizeof(DataHeader),
+           header->dataLength - sizeof(DataHeader), 0);
+      LoginResult* result = (LoginResult*)recvBuf;
+      printf(
+          "INFO|Receive login result from server, data length: %d, result "
+          "code: %d.\n",
+          result->dataLength, result->result);
+    } break;
+    case CMD::CMD_LOGOUT_RESULT: {
+      recv(_cSock, recvBuf + sizeof(DataHeader),
+           header->dataLength - sizeof(DataHeader), 0);
+      LogoutResult* result = (LogoutResult*)recvBuf;
+      printf(
+          "INFO|Receive logout result from server, data length: %d, result "
+          "code: "
+          "%d\n",
+          result->dataLength, result->result);
+    } break;
+    case CMD::CMD_NEW_USER_JOIN: {
+      recv(_cSock, recvBuf + sizeof(DataHeader),
+           header->dataLength - sizeof(DataHeader), 0);
+      NewUserJoin* newJoin = (NewUserJoin*)recvBuf;
+      printf(
+          "INFO|Receive new user join from server, data length: %d, new user: "
+          "%d.\n",
+          newJoin->dataLength, newJoin->sock);
+    } break;
+    default: {
+      DataHeader header = {0, CMD_ERROR};
+      printf("INFO|Receive unknow command.\n");
+    } break;
   }
   return 0;
 }
 
 bool g_Run = true;
-void readCmd(SOCKET _sock)
-{
+void readCmd(SOCKET _sock) {
   char cmdBuf[1024] = {};
   int count = 0;
-  while (true)
-  {
-    printf("Please enter a command%d:\n", count++);
+  while (true) {
+    printf("Please enter command%d:\n", count++);
     memset(cmdBuf, 0, 1024);
 // 3. 读取指令
 // 如果想要读取带有空格的字符串，此处不能直接使用scanf和scanf_s，它们读取到空格就会停止
@@ -113,14 +98,11 @@ void readCmd(SOCKET _sock)
     scanf("%[^\n]%*c", cmdBuf);
 #endif
     DataHeader retHeader = {};
-    if (strcmp(cmdBuf, "exit") == 0)
-    {
+    if (strcmp(cmdBuf, "exit") == 0) {
       printf("Exiting...\n");
       g_Run = false;
       return;
-    }
-    else if (strcmp(cmdBuf, "login") == 0)
-    {
+    } else if (strcmp(cmdBuf, "login") == 0) {
       Login login;
 #ifdef WIN32
       strcpy_s(login.userName, "xiaoyao");
@@ -129,10 +111,8 @@ void readCmd(SOCKET _sock)
       strcpy(login.userName, "xiaoyao");
       strcpy(login.password, "123456");
 #endif
-      send(_sock, (const char *)&login, sizeof(Login), 0);
-    }
-    else if (strcmp(cmdBuf, "logout") == 0)
-    {
+      send(_sock, (const char*)&login, sizeof(Login), 0);
+    } else if (strcmp(cmdBuf, "logout") == 0) {
       Logout logout;
 #ifdef WIN32
       strcpy_s(logout.userName, "xiaoyao");
@@ -140,69 +120,34 @@ void readCmd(SOCKET _sock)
       strcpy(logout.userName, "xiaoyao");
 #endif
       DataHeader dataHeader = {sizeof(Logout), CMD_LOGOUT};
-      send(_sock, (const char *)&logout, sizeof(Logout), 0);
-    }
-    else
-    {
+      send(_sock, (const char*)&logout, sizeof(Logout), 0);
+    } else {
       printf("Unsupported command, please retry.\n");
       continue;
     }
   }
 }
 
-int main()
-{
-#ifdef WIN32
-  // 启动WindowsSocket 2.x 的网络编程环境
-  WORD ver = MAKEWORD(2, 2);
-  WSADATA dat;
-  if (WSAStartup(ver, &dat) != NO_ERROR)
-  {
-    printf("ERROR|%s:%d: WSAStartup failed with error: %u.\n", __FILE__,
-           __LINE__, WSAGetLastError());
-    return -1;
-  }
-  else
-  {
-    printf("SUCCESS|%s:%d: WSAStartup success.\n", __FILE__, __LINE__);
-  }
-#endif
+int main() {
+  initSocket();
   // 1. 建立一个socket
   SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (_sock == INVALID_SOCKET)
-  {
-#ifdef WIN32
-    printf("ERROR|%s:%d: create socket failed with error: %u.\n", __FILE__,
-           __LINE__, WSAGetLastError());
-    WSACleanup();
-#else
-    printf("ERROR|%s:%d: create socket failed with error: %d.\n", __FILE__, __LINE__, errno);
-#endif
-    return -1;
-  }
-  else
-  {
-    printf("SUCCESS|%s:%d: create socket success.\n", __FILE__, __LINE__);
+  if (_sock == INVALID_SOCKET) {
+    logError(__FILE__, __LINE__, "socket");
+    exitProcess(_sock);
+  } else {
+    logSuccess(__FILE__, __LINE__, "socket");
   }
   // 2. 连接服务器 connect
   sockaddr_in _sin = {};
   _sin.sin_family = AF_INET;
   _sin.sin_port = htons(16642);
   inet_pton(AF_INET, SERVER_IP, &_sin.sin_addr);
-  if (connect(_sock, (sockaddr *)&_sin, sizeof(sockaddr_in)) == SOCKET_ERROR)
-  {
-#ifdef WIN32
-    printf("ERROR|%s:%d: connect failed with error: %u\n", __FILE__, __LINE__,
-           WSAGetLastError());
-    WSACleanup();
-#else
-    printf("ERROR|%s:%d: connect failed with error: %d\n", __FILE__, __LINE__, errno);
-#endif
-    return -1;
-  }
-  else
-  {
-    printf("SUCCESS|%s:%d: connect success.\n", __FILE__, __LINE__);
+  if (connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in)) == SOCKET_ERROR) {
+    logError(__FILE__, __LINE__, "connect");
+    exitProcess(_sock);
+  } else {
+    logSuccess(__FILE__, __LINE__, "connect");
   }
 
   // 启动线程
@@ -211,53 +156,35 @@ int main()
   cmdThread.detach();
 
   fd_set readSet = {};
-  while (g_Run)
-  {
+  while (g_Run) {
+    // 由于只有一个socket，因此就不使用initFdSets()函数了
     FD_ZERO(&readSet);
     FD_SET(_sock, &readSet);
     // linux下的select返回后会将超时参数置空，因此需要在每次select之前都需要重新设置超时参数
     timeval t = {1, 0};
     // 考虑到只有一个需要监听的套接字，此处就不清空以及重新设置readSet了
     // linux 下的select第一个参数必须设置为三个描述符集中的 fdmax + 1
-    int ret = select(_sock + 1, &readSet, nullptr, nullptr, &t);
-    if (ret < 0)
-    {
+    auto selectRet = select(_sock + 1, &readSet, nullptr, nullptr, &t);
+    if (selectRet < 0) {
       // 考虑到接收新客户端连接的消息是非必需功能，因此此处出错不中断程序，而是继续向下执行。
-#ifdef WIN32
-      printf("ERROR|%s:%d: select error: %u.\n", __FILE__, __LINE__,
-             GetLastError());
-#else
-      printf("ERROR|%s:%d: select error: %d.\n", __FILE__, __LINE__, errno);
-#endif
-    }
-    else if (ret > 0)
-    {
-      printf("select returned a value greater than zero.\n");
-      if (FD_ISSET(_sock, &readSet))
-      {
+      logError(__FILE__, __LINE__, "select");
+    } else if (selectRet > 0) {
+      if (FD_ISSET(_sock, &readSet)) {
         // 读取消息放到这里
         // doRead 返回-1说明网络连接出了问题或者服务器关闭了，此时直接退出
-        if (doRead(_sock) == -1)
-        {
+        if (doRead(_sock) == -1) {
           break;
         }
       }
-    }
-    else
-    {
+    } else {
       printf("select returned 0.\n");
     }
-    // printf("INFO|Handle other business...\n");
+    printf("INFO|Handle other business...\n");
   }
-
-// 4. 关闭套接字closesocket
+  cleanSocket();
+  closeSocket(_sock);
 #ifdef WIN32
-  closesocket(_sock);
-  // 清理WindowsSocket的网络变成环境
-  WSACleanup();
-#else
-  close(_sock);
-#endif
   system("pause");
+#endif
   return 0;
 }
--- v1.0/Server/main.cpp
+++ v1.0/Server/main.cpp
@@ -6,15 +6,29 @@
  * 例子就是winsock2.h和windows.h之间有关于_WINSOCKAPI_的重复定义，如果定义了上面的宏编译
  * 就会避免出现重定义，不同的链接的错误
  */
+#ifdef WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <WS2tcpip.h>
 #include <WinSock2.h>
 #include <Windows.h>
+#define ADDR_LEN int
+#else
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#define SOCKET int
+#define INVALID_SOCKET (SOCKET)(~0)
+#define SOCKET_ERROR (-1)
+#define ADDR_LEN socklen_t
+#endif
 
 #include <cstdio>
 #include <unordered_set>
+// #include <cstdlib>
 
 #include "../Common/Data.h"
+#include "../Common/cross_platform_socket.h"
+#include "../Common/log.h"
 
 // 加入WindowsSocket的静态库，此处采取第二种方法，为工程添加附加依赖项
 // #pragma comment(lib, "ws2_32.lib")
@@ -24,8 +38,39 @@ std::unordered_set<SOCKET> readSockets;
 std::unordered_set<SOCKET> writeSockets;
 std::unordered_set<SOCKET> expSockets;
 
-// 规定当客户端退出时，返回
-// -1，此时需要在主处理流程中将通信socket从相应的集合中移除
+void doAccept(SOCKET listenSock) {
+  sockaddr_in client = {};
+  ADDR_LEN addrLen = sizeof(client);
+
+  auto _cSock = accept(listenSock, (sockaddr*)&client, &addrLen);
+  if (_cSock == INVALID_SOCKET) {
+    logError(__FILE__, __LINE__, "accept");
+  } else {
+    logSuccess(__FILE__, __LINE__, "accept");
+    // 此处不可以使用 inet_ntoa()，需使用 inet_ntop 替代
+    char ipStr[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client.sin_addr, ipStr, INET_ADDRSTRLEN);
+    printf("New client access. socket = %llu, IP = %s, Port = %u.\n", _cSock,
+           ipStr, client.sin_port);
+    // v0.8: 将新增加的客户端群发给目前已有的所有客户端
+    NewUserJoin newUser;
+    newUser.sock = _cSock;
+    for (SOCKET sock : readSockets) {
+      // v1.0 linux下服务端不允许send函数给监听socket发送数据
+      if (sock == listenSock)
+        continue;
+      send(sock, (const char* const) & newUser, sizeof(NewUserJoin), 0);
+    }
+    // 将新连接的通信socket添加到 read_sockets, write_sockets,
+    // exp_sockets;
+    readSockets.insert(_cSock);
+    writeSockets.insert(_cSock);
+    expSockets.insert(_cSock);
+  }
+}
+
+// 规定当客户端退出时，返回 -1
+// 此时需要在主处理流程中将通信socket从相应的集合中移除
 int doRead(SOCKET _cSock) {
   char recvBuf[1024] = {};
   // 5. 接收客户端数据，如果接收到的数据长度为0，则退出函数
@@ -33,57 +78,57 @@ int doRead(SOCKET _cSock) {
     printf("Client(%llu) has exit, task end.\n", _cSock);
     return -1;
   }
-  DataHeader *header = (DataHeader *)recvBuf;
+  DataHeader* header = (DataHeader*)recvBuf;
   switch (header->cmd) {
     case CMD::CMD_LOGIN: {
       // 由于Login中包含了DataHeader，第二次接收时，需要进行内存偏移
       recv(_cSock, recvBuf + sizeof(DataHeader),
            header->dataLength - sizeof(DataHeader), 0);
-      Login *login = (Login *)recvBuf;
+      Login* login = (Login*)recvBuf;
       printf(
-          "INFO|Receive command login from socket(%llu), data length: %d, "
-          "userName: %s.\n",
+          "INFO|Receive command login from socket(%llu), data length: "
+          "%d, userName: %s.\n",
           _cSock, login->dataLength, login->userName);
       // 忽略判断用户名密码
       // 给客户端返回登陆成功的信息
       LoginResult result;
-      send(_cSock, (const char *const) & result, sizeof(LoginResult), 0);
+      send(_cSock, (const char* const) & result, sizeof(LoginResult), 0);
     } break;
     case CMD::CMD_LOGOUT: {
       recv(_cSock, recvBuf + sizeof(DataHeader),
            header->dataLength - sizeof(DataHeader), 0);
-      Logout *logout = (Logout *)recvBuf;
+      Logout* logout = (Logout*)recvBuf;
       printf(
-          "INFO|Receive command logout from socket(%llu), data length: %d, "
+          "INFO|Receive command logout from socket(%llu), data length: "
+          "%d, "
           "userName: %s.\n",
           _cSock, logout->dataLength, logout->userName);
       LogoutResult result;
-      send(_cSock, (const char *const) & result, sizeof(LogoutResult), 0);
+      send(_cSock, (const char* const) & result, sizeof(LogoutResult), 0);
     } break;
     default: {
       DataHeader header = {0, CMD_ERROR};
       printf("INFO|Receive unknow command.\n");
-      send(_cSock, (const char *const) & header, sizeof(DataHeader), 0);
+      send(_cSock, (const char* const) & header, sizeof(DataHeader), 0);
     } break;
   }
   return 0;
 }
 
 int main() {
-  // 先定义一个存储错误码的变量
-  DWORD errCode = 0;
-  // 启动WindowsSocket 2.x 的网络编程环境
-  WORD ver = MAKEWORD(2, 2);
-  WSADATA dat;
-  if (WSAStartup(ver, &dat) != NO_ERROR) {
-    printf("ERROR|%s:%d: WSAStartup error.\n", __FILE__, __LINE__);
-    return 1;
-  }
+  // 封装成了一个函数，如果是 Windows 环境，则此函数的函数体非空
+  initSocket();
   // 1. 建立一个socket(监听socket)
-  SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  SOCKET listenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (listenSock == INVALID_SOCKET) {
+    logError(__FILE__, __LINE__, "socket");
+    exitProcess(listenSock);
+  } else {
+    logSuccess(__FILE__, __LINE__, "socket");
+  }
   // 设置端口重用
   int on = 1;
-  setsockopt(_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
+  setsockopt(listenSock, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
   // 2. 绑定接收客户端连接的端口 bind
   sockaddr_in _sin = {};
   _sin.sin_family = AF_INET;
@@ -93,115 +138,68 @@ int main() {
   // _sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
   // VC++构造socket地址的新写法
   inet_pton(AF_INET, "127.0.0.1", &_sin.sin_addr);
-  if (bind(_sock, (sockaddr *)&_sin, sizeof(sockaddr_in)) == SOCKET_ERROR) {
-    errCode = GetLastError();
-    printf("ERROR|%s:%d: bind fail, error: %u.\n", __FILE__, __LINE__, errCode);
-    WSACleanup();
-    ExitProcess(errCode);
+  if (bind(listenSock, (sockaddr*)&_sin, sizeof(sockaddr_in)) == SOCKET_ERROR) {
+    logError(__FILE__, __LINE__, "bind");
+    exitProcess(listenSock);
   } else {
-    printf("SUCCESS|%s:%d: bind success.\n", __FILE__, __LINE__);
+    logSuccess(__FILE__, __LINE__, "bind");
   }
   // 3. 监听网络端口 listen
-  if (listen(_sock, 128) == SOCKET_ERROR) {
-    errCode = GetLastError();
-    printf("ERROR|%s:%d: listen fail, error: %u.\n", __FILE__, __LINE__,
-           errCode);
-    WSACleanup();
-    ExitProcess(errCode);
+  if (listen(listenSock, 128) == SOCKET_ERROR) {
+    logError(__FILE__, __LINE__, "listen");
+    exitProcess(listenSock);
   } else {
-    printf("SUCCESS|%s:%d: listen success.\n", __FILE__, __LINE__);
+    logSuccess(__FILE__, __LINE__, "listen");
   }
   // 完成监听后，将_sock添加到 read_sockets, write_sockets, exp_sockets;
-  readSockets.insert(_sock);
-  writeSockets.insert(_sock);
-  expSockets.insert(_sock);
+  readSockets.insert(listenSock);
+  writeSockets.insert(listenSock);
+  expSockets.insert(listenSock);
 
-  // 4. 等待接收客户端连接accept
-  sockaddr_in client = {};
-  int nAddrLen = sizeof(client);
-  // 此变量用于接收新的通信socket
-  SOCKET _cSock = INVALID_SOCKET;
-  char const *msg = nullptr;
+  char const* msg = nullptr;
   int recvLen = 0;
   char recvBuf[1024] = {};
   // 伯克利socket
   fd_set readSet = {};
   fd_set writeSet = {};
   fd_set expSet = {};
-
-  // v0.8: 给select传递一个超时参数(最后一个参数)
-  timeval t = {5, 0};
+  SOCKET maxFd = 0;
 
   while (true) {
-    // 经过实测，以及查阅代码
-    // 发现FD_ZERO仅仅修改fd_set.fd_count，并不会修改fd_set.fd_array
-    // 所以此处不采取FD_ZERO的方式，使用memset将其恢复至初始状态
-    FD_ZERO(&readSet);
-    FD_ZERO(&writeSet);
-    FD_ZERO(&expSet);
-    // memset(&readSet, 0, sizeof(fd_set));
-    // memset(&writeSet, 0, sizeof(fd_set));
-    // memset(&expSet, 0, sizeof(fd_set));
-    // 将需要监控的socket添加到read_set、write_set、exp_set中
-    for (auto var : readSockets) {
-      FD_SET(var, &readSet);
-    }
-    for (auto var : writeSockets) {
-      FD_SET(var, &writeSet);
-    }
-    for (auto var : expSockets) {
-      FD_SET(var, &expSet);
-    }
-
+    // v0.8: 给select传递一个超时参数(最后一个参数)
+    timeval t = {1, 0};
+    SOCKET maxFd = initFdSets(&readSet, &readSockets, &writeSet, &writeSockets,
+                              &expSet, &expSockets);
     // nfds是一个整数值，三个fd_set中所有描述符最大值+1，在Windows中这个参数可以写0
     // select 执行出错，即退出循环。
-    //if (select(_sock + 1, &readSet, &writeSet, &expSet, &t) < 0) {
-    if (select(_sock + 1, &readSet, nullptr, &expSet, &t) < 0) {
-      printf("ERROR|The select task ends.\n");
-      printf("ERROR|select error: %d.\n", GetLastError());
+    auto selectRet = select(maxFd + 1, &readSet, nullptr, nullptr, &t);
+    if (selectRet < 0) {
+      logError(__FILE__, __LINE__, "select");
       break;
     }
-    // 检测到_sock可读，说明有新的客户端连接
-    if (FD_ISSET(_sock, &readSet)) {
-      _cSock = accept(_sock, (sockaddr *)&client, &nAddrLen);
-      if (_cSock == INVALID_SOCKET) {
-        printf(
-            "ERROR|%s:%d: accept error, get a invalid client socket, error: "
-            "%u.\n",
-            __FILE__, __LINE__, GetLastError());
-      } else {
-        // 此处不可以使用 inet_ntoa()，需使用 inet_ntop 替代
-        char ipStr[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &client.sin_addr, ipStr, INET_ADDRSTRLEN);
-        printf("New client access. socket = %llu, IP = %s, Port = %u.\n",
-               _cSock, ipStr, client.sin_port);
-        // v0.8: 将新增加的客户端群发给目前已有的所有客户端
-        NewUserJoin newUser;
-        newUser.sock = _cSock;
-        for (SOCKET sock : readSockets) {
-          send(sock, (const char *const) & newUser, sizeof(NewUserJoin), 0);
-        }
-        // 将新连接的通信socket添加到 read_sockets, write_sockets, exp_sockets;
-        readSockets.insert(_cSock);
-        writeSockets.insert(_cSock);
-        expSockets.insert(_cSock);
+    // selectRet == 0 时直接处理其它业务
+    if (selectRet > 0) {
+      // 检测到_sock可读，说明有新的客户端连接
+      if (FD_ISSET(listenSock, &readSet)) {
+        doAccept(listenSock);
       }
-    }
-    // 处理可读的通信socket
-    for (int i = 0; i < readSet.fd_count; i++) {
-      if (readSet.fd_array[i] == _sock) continue;
-      // 如果当前客户端退出，doRet == -1
-      // 需要将对应的socket其从相应的集合中移除
-      int doRet = doRead(readSet.fd_array[i]);
-      if (doRet == -1) {
-        // 通信socket也需要关闭，在之前的版本中
-        // 都没有关闭，这是不对的
-        // 一般教程或者书籍中将其称之为客户端socket
-        // 不过我觉得叫作通信socket更加合适
-        closesocket(readSet.fd_array[i]);
-        readSockets.erase(readSet.fd_array[i]);
-        writeSockets.erase(readSet.fd_array[i]);
-        expSockets.erase(readSet.fd_array[i]);
+      // 处理可读的通信socket
+      for (auto var : readSockets) {
+        if (var == listenSock)
+          continue;
+        // 如果当前客户端退出，doRet == -1
+        // 需要将对应的socket其从相应的集合中移除
+        int doRet = doRead(var);
+        if (doRet == -1) {
+          // 通信socket也需要关闭，在之前的版本中
+          // 都没有关闭，这是不对的
+          // 一般教程或者书籍中将其称之为客户端socket
+          // 不过我觉得叫作通信socket更加合适
+          closeSocket(var);
+          readSockets.erase(var);
+          writeSockets.erase(var);
+          expSockets.erase(var);
+        }
       }
     }
     printf("INFO|Handle other business...\n");
@@ -209,11 +207,11 @@ int main() {
   // 关闭所有的socket
   // 一般来说是走不到这一步的，但是为了容错，这里添加上
   for (auto sock : readSockets) {
-    closesocket(sock);
+    closeSocket(sock);
   }
-  // _sock也在上面一步中处理了，此处注释掉相关代码
-  // closesocket(_sock);
+  // listenSock也在上面一步中处理了，此处注释掉相关代码
+  // closesocket(listenSock);
   // 清理WindowsSocket的网络变成环境
-  WSACleanup();
+  cleanSocket();
   return 0;
 }
